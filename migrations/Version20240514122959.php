4be57f3c3101594cdf258a04fb3b7b3f584452f7
Natalia Meshkova <nataliameshkova@192.168.1.11>
2024-05-14 14:31:09 +0200

    fixes

---
+++ migrations/Version20240514122959.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240514122959 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP INDEX idx_parent_id');
+        $this->addSql('ALTER TABLE task ALTER priority DROP DEFAULT');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE task ALTER priority SET DEFAULT 1');
+        $this->addSql('CREATE INDEX idx_parent_id ON task (parent_id)');
+    }
+}
